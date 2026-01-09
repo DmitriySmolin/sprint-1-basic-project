1ba53e3aee488657f6719e0b66e56544f7373c07
Смолин Дмитрий Эдуардович <DSmolin@srt-llc.ru>
2026-01-09 18:38:53 +0400

    init: project structure and types

---
+++ types.go
@@ -0,0 +1,23 @@
+package main
+
+// Stock — остатки ингредиентов
+type Stock map[string] int
+
+// Recipe — рецепт напитка
+type Recipe struct {
+	Price int
+	Ingredients map[string] int
+	Steps []string
+}
+
+// Stats — статистика продаж
+type Stats struct {
+	Orders int
+	Revenue int
+}
+
+// State — сохраняемое состояние
+type State struct {
+	Stock Stock `json:"stock"`
+	Stats Stats `json:"stats"`
+}

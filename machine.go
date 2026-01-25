cc8f81f0d032f0e3ba1ea1c3902d8c88a1069175
Смолин Дмитрий Эдуардович <DSmolin@srt-llc.ru>
2026-01-25 22:14:26 +0400

    feat: add CLI argument parsing

--- machine.go
+++ machine.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+const stateFile = "coffee_state.json"
+
 type Machine struct {
 	Stock   Stock
 	Recipes map[string]Recipe
@@ -12,4 +20,33 @@ func NewMachine() *Machine {
 		Recipes: recipes,
 	}
 	return m
+}
+
+func (m *Machine) Load() error {
+	data, err := os.ReadFile(stateFile)
+	if os.IsNotExist(err) {
+		return nil
+	}
+	if err != nil {
+		return err
+	}
+	var state State
+	if err := json.Unmarshal(data, &state); err != nil {
+		return err
+	}
+	m.Stock = state.Stock
+	m.Stats = state.Stats
+	return nil
+}
+
+func (m *Machine) Save() error {
+	state := State{Stock: m.Stock, Stats: m.Stats}
+	data, err := json.Marshal(state)
+	if err != nil {
+		return err
+	}
+	return os.WriteFile(stateFile, data, 0644)
+}
+func (m *Machine) PrintStats() {
+	fmt.Printf("orders=%d revenue=%d\n", m.Stats.Orders, m.Stats.Revenue)
 }
--- main.go
+++ main.go
@@ -1,30 +1,108 @@
 package main
 
-import "github.com/k0kubun/pp/v3"
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
 
+func printUsage() {
+	fmt.Println("usage")
+}
 func main() {
 	machine := NewMachine()
+	if err := machine.Load(); err != nil {
+		fmt.Println("ошибка")
+		os.Exit(3)
+	}
+	args := os.Args[1:]
 
-	// fmt.Println(machine.Recipes["espresso"].Price)
-
-	// machine.GetStock()
-
-	// machine.AddStock("beans", 2)
-	// pp.Println(machine)
-
-	machine.SetStock("water", 3)
-	// pp.Println(machine)
-
-	machine.AddStock("beans", 10)
-	machine.AddStock("water", 50)
-
-	// Варим espresso с оплатой 150
-	err := machine.Brew("espresso", 150)
-	if err != nil {
-		pp.Println("Ошибка:", err)
+	if len(args) == 0 {
+		printUsage()
+		os.Exit(2)
 	}
 
-	machine.Menu()
-	machine.GetStats()
+	var needSave bool
 
+	switch args[0] {
+	case "help":
+		printUsage()
+		os.Exit(0)
+
+	case "menu":
+		machine.Menu()
+
+	case "stock":
+		if len(args) < 2 {
+			printUsage()
+			os.Exit(2)
+		}
+		switch args[1] {
+		case "get":
+			machine.GetStock()
+		case "add":
+			if len(args) < 4 {
+				printUsage()
+				os.Exit(2)
+			}
+			qty, err := strconv.Atoi(args[3])
+			if err != nil {
+				fmt.Println("некорректные параметры")
+				os.Exit(1)
+			}
+			if err := machine.AddStock(args[2], qty); err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+			needSave = true
+		case "set":
+			if len(args) < 4 {
+				printUsage()
+				os.Exit(2)
+			}
+			qty, err := strconv.Atoi(args[3])
+			if err != nil {
+				fmt.Println("некорректные параметры")
+				os.Exit(1)
+			}
+			if err := machine.SetStock(args[2], qty); err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+			needSave = true
+		default:
+			printUsage()
+			os.Exit(2)
+		}
+
+	case "brew":
+		if len(args) < 4 || args[2] != "--pay" {
+			printUsage()
+			os.Exit(2)
+		}
+		payment, err := strconv.Atoi(args[3])
+		if err != nil {
+			printUsage()
+			os.Exit(2)
+		}
+		if err := machine.Brew(args[1], payment); err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		needSave = true
+
+	case "stats":
+		machine.PrintStats()
+
+	default:
+		printUsage()
+		os.Exit(2)
+	}
+
+	if needSave {
+		if err := machine.Save(); err != nil {
+			fmt.Println("ошибка")
+			os.Exit(3)
+		}
+	}
 }
